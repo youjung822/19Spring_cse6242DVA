a57e83d7f7314250d442b045181efd1d61d8a81f
ydoow <ydoow.earth@gmail.com>
2019-03-12 19:35:24 +1100

    master: added select category and arrange better modulization by Wood

---
+++ js/userInput.js
@@ -0,0 +1,47 @@
+$( function() {
+	
+	var dialog, form,	name = $( "#form-name" );					
+
+	function processUserInput() {							
+																	
+			dialog.dialog( "close" );				
+			
+			$("#divMain").removeClass("transparent");
+			$("#divMain").addClass("fullyVisible");															
+			
+			var selectedBudget = "";
+			if ($("#budget-low").hasClass("ui-selected"))
+				selectedBudget = "low";
+			else if ($("#budget-mid").hasClass("ui-selected"))
+				selectedBudget = "mid";
+			else if ($("#budget-high").hasClass("ui-selected"))
+				selectedBudget = "high";
+															
+			$("#name").val(selectedBudget);
+			
+			
+	}
+
+	dialog = $("#inputDialog").dialog({
+		autoOpen: false,
+		height: 430,
+		width: 600,
+		modal: true,
+		buttons: {
+			"Submit": processUserInput
+		},
+		close: function() {
+			form[0].reset();					
+		}
+	});
+
+	form = dialog.find( "form" ).on( "submit", function( event ) {
+		event.preventDefault();				
+		processUserInput();
+	});
+
+	$(document).ready(function(){ 
+		dialog.dialog( "open" );
+	})
+
+} );		
