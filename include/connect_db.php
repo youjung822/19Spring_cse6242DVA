01ae554346071a94fb212893c75a826f6e0234cd
ydoow <ydoow.earth@gmail.com>
2019-03-22 16:38:33 +1100

    master: added category matching

--- include/connect_db.php
+++ include/connect_db.php
@@ -1,18 +1,32 @@
 <?php
 					
-    $username = "tutorial_user"; 
-    $password = "12341234";   
-    $host = "mytestdb.cen3rhkl2zlv.us-east-1.rds.amazonaws.com";
+    $username = "root"; 
+    $password = "6242";   
+    $host = "localhost";
     $database="trip.analyzer";
     
     $connection = mysqli_connect($host, $username, $password, $database);    
 
-    $myquery = "SELECT c.rank, c.city, l.lat, l.lng, a.rk as arrivals_rank, r.rk as receipts_rank
+    $myquery = "SELECT c.rank, c.city, l.lat, l.lng, a.rk as arrivals_rank, r.rk as receipts_rank, s_poi.*
 		FROM `cities` c
 		join cities_sygic_mapping m on c.rank = m.city_id 
 		join sygic_city_location l on m.sygic_id = l.cityid  
 		join `arrivals_processed` a on c.rank = a.id 
 		join `receipts_processed` r on c.rank = r.id 
+		join 
+		(select 
+			cityid,
+			sum(discovering) as discovering, 
+			sum(doing_sports) as sports,
+			sum(eating) as eating,
+			sum(going_out) as nightlife,
+			sum(hiking) as hiking,
+			sum(relaxing) as relaxing,
+			sum(shopping) as shopping,
+			sum(sightseeing) as sightseeing,
+			sum(playing) as family
+		from `sygic_poi` 
+		group by cityid) s_poi on s_poi.cityid = m.sygic_id
 		order by c.rank";
     $query = mysqli_query($connection, $myquery);
     
