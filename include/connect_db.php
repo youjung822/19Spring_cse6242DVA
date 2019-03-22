ddedf6f188dbca270ee1ba178a56d9e45ac7b32b
ydoow <ydoow.earth@gmail.com>
2019-03-22 16:41:48 +1100

    master: updated connection string to aws

--- include/connect_db.php
+++ include/connect_db.php
@@ -1,8 +1,8 @@
 <?php
 					
-    $username = "root"; 
-    $password = "6242";   
-    $host = "localhost";
+    $username = "tutorial_user"; 
+    $password = "12341234";   
+    $host = "mytestdb.cen3rhkl2zlv.us-east-1.rds.amazonaws.com";
     $database="trip.analyzer";
     
     $connection = mysqli_connect($host, $username, $password, $database);    
